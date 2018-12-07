844c143fd17d8465fe400609d91e40e86ddd0363
marcuspereiragithub <vinnycoast@gmail.com>
2018-12-07 15:38:28 -0300

    Using nyc to tests

--- tests/fizzbuzz.spec.js
+++ tests/fizzbuzz.spec.js
@@ -3,20 +3,20 @@ import fizzBuzz from '../src/fizzbuzz_main.js';
 
 describe('FizzBuzz', () => {
   it('should return `FizzBuzz` when multiple of 3 and 5', () => {
-      expect(fizzBuzz(15)).to.be.equal('FizzBuzz');
+    expect(fizzBuzz(15)).to.be.equal('FizzBuzz');
   });
   it('should return `Fizz` when multiple of 3', () => {
-      expect(fizzBuzz(3)).to.be.equal('Fizz');
-      expect(fizzBuzz(6)).to.be.equal('Fizz');
+    expect(fizzBuzz(3)).to.be.equal('Fizz');
+    expect(fizzBuzz(6)).to.be.equal('Fizz');
   });
   it('should return `Buzz` when multiple of 5', () => {
-      expect(fizzBuzz(5)).to.be.equal('Buzz');
-      expect(fizzBuzz(20)).to.be.equal('Buzz');
+    expect(fizzBuzz(5)).to.be.equal('Buzz');
+    expect(fizzBuzz(20)).to.be.equal('Buzz');
   });
   it('should return the number when no multiple', () => {
-      expect(fizzBuzz(7)).to.be.equal(7);
+    expect(fizzBuzz(7)).to.be.equal(7);
   });
   it('should return 0 when 0', () => {
-      expect(fizzBuzz(0)).to.be.equal(0);
+    expect(fizzBuzz(0)).to.be.equal(0);
   });
 });
