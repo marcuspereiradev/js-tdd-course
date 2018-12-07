16dc805280d3a0cb3bbf2fd7455eaaea6f327ed6
marcuspereiragithub <vinnycoast@gmail.com>
2018-12-07 15:34:10 -0300

    Using nyc to tests

--- tests/calc.spec.js
+++ tests/calc.spec.js
@@ -1,54 +1,54 @@
-import { expect } from 'chai';
-import { sum, sub, mult, div } from '../src/main.js';
+// import { expect } from 'chai';
+// import { sum, sub, mult, div } from '../src/calc_main.js';
 
-describe('Calc', () => {
+// describe('Calc', () => {
 
-    describe('Smoke Tests', () => {
-        it('should exist the method `sum`', () => {
-            expect(sum).to.exist;
-            expect(sum).to.be.a('function');
-        });
-        it('should exist the method `sub`', () => {
-            expect(sub).to.exist;
-            expect(sub).to.be.a('function');
-        });
-        it('should exist the method `mult`', () => {
-            expect(mult).to.exist;
-            expect(mult).to.be.a('function');
-        });
-        it('should exist the method `div`', () => {
-            expect(div).to.exist;
-            expect(div).to.be.a('function');
-        });
-    });
+//   describe('Smoke Tests', () => {
+//     it('should exist the method `sum`', () => {
+//         expect(sum).to.exist;
+//         expect(sum).to.be.a('function');
+//     });
+//     it('should exist the method `sub`', () => {
+//         expect(sub).to.exist;
+//         expect(sub).to.be.a('function');
+//     });
+//     it('should exist the method `mult`', () => {
+//         expect(mult).to.exist;
+//         expect(mult).to.be.a('function');
+//     });
+//     it('should exist the method `div`', () => {
+//         expect(div).to.exist;
+//         expect(div).to.be.a('function');
+//     });
+//   });
 
-    describe('Sum', () => {
-        it('should return 10 when `sum(6, 4)`', () => {
-            expect(sum(6, 4)).to.be.equal(10);
-        });
-    });
+//   describe('Sum', () => {
+//     it('should return 10 when `sum(6, 4)`', () => {
+//         expect(sum(6, 4)).to.be.equal(10);
+//     });
+//   });
 
-    describe('Sub', () => {
-        it('should return 4 when `sub(20, 16)`', () => {
-            expect(sub(20, 16)).to.be.equal(4);
-        });
-        it('should return the negative value -110 when `sub(10, 120)`', () => {
-            expect(sub(10, 120)).to.be.equal(-110);
-        });
-    });
+//   describe('Sub', () => {
+//     it('should return 4 when `sub(20, 16)`', () => {
+//         expect(sub(20, 16)).to.be.equal(4);
+//     });
+//     it('should return the negative value -110 when `sub(10, 120)`', () => {
+//         expect(sub(10, 120)).to.be.equal(-110);
+//     });
+//   });
 
-    describe('Mult', () => {
-        it('should return 1000 when `mult(4, 250)`', () => {
-            expect(mult(4, 250)).to.be.equal(1000);
-        });
-    });
+//   describe('Mult', () => {
+//     it('should return 1000 when `mult(4, 250)`', () => {
+//         expect(mult(4, 250)).to.be.equal(1000);
+//     });
+//   });
 
-    describe('Div', () => {
-        it('should return 50 when `div(150, 3)`', () => {
-            expect(div(150, 3)).to.be.equal(50);
-        });
-        it('should return `Its not possible division by zero` when `div(20, 0)`', () => {
-            expect(div(20, 0)).to.be.equal('Its not possible division by zero');
-        });
-    });
-});
+//   describe('Div', () => {
+//     it('should return 50 when `div(150, 3)`', () => {
+//         expect(div(150, 3)).to.be.equal(50);
+//     });
+//     it('should return `Its not possible division by zero` when `div(20, 0)`', () => {
+//         expect(div(20, 0)).to.be.equal('Its not possible division by zero');
+//     });
+//   });
+// });
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,40 +1,40 @@
 // describe('Main', function() {
-//     var arr;
-  
-//     // roda uma vez, antes do bloco
-//     before(function() {
-//         // inicia uma conexão no banco
-//         // criar um conjunto de dados
-//     });
-  
-//     // roda uma vez, depois do bloco
-//     after(function() {
-//         // fecha conexão do banco
-//         // apagar esse conjunto de dados
-//     });
-  
-//     // roda todas as vezes, antes de CADA bloco
-//     beforeEach(function() {
-//         arr = [1, 2, 3];
-//     });
-  
-//     // roda todas as vezes, depois de CADA bloco
-//     afterEach(function() {
-  
-//     });
-  
-//     it('should have a size of 4 when push another value to the array', function() {
-//         arr.push(4);
-//         console.log(arr.length); // 4
-//     });
-  
-//     it('should remove the value 3 when use pop in the array', function() {
-//         console.log(arr.pop() === 3); // true
-//     });
-  
-//     it('should have a size of 2 when pop a value from the array', function() {
-//         arr.pop();
-//         console.log(arr.length); // 2
-//     });
-  
+//   var arr;
+
+//   // roda uma vez, antes do bloco
+//   before(function() {
+//     // inicia uma conexão no banco
+//     // criar um conjunto de dados
 //   });
+
+//   // roda uma vez, depois do bloco
+//   after(function() {
+//     // fecha conexão do banco
+//     // apagar esse conjunto de dados
+//   });
+
+//   // roda todas as vezes, antes de CADA bloco
+//   beforeEach(function() {
+//     arr = [1, 2, 3];
+//   });
+
+//   // roda todas as vezes, depois de CADA bloco
+//   afterEach(function() {
+
+//   });
+
+//   it('should have a size of 4 when push another value to the array', function() {
+//     arr.push(4);
+//     console.log(arr.length); // 4
+//   });
+
+//   it('should remove the value 3 when use pop in the array', function() {
+//     console.log(arr.pop() === 3); // true
+//   });
+
+//   it('should have a size of 2 when pop a value from the array', function() {
+//     arr.pop();
+//     console.log(arr.length); // 2
+//   });
+
+// });
